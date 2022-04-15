5db0cd0e170279e4086e696e0d5e7bec73ef5f40
Chris Brownlee <76001262+FearsRedemption@users.noreply.github.com>
2022-04-15 17:41:39 -0500

    [FearsMod] - renamed the items to Magitek

--- src/main/java/net/fearsredemption/fearsmod/FearsMod.java
+++ src/main/java/net/fearsredemption/fearsmod/FearsMod.java
@@ -5,12 +5,14 @@ import net.fearsredemption.fearsmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.fearsredemption.fearsmod.item.ModItems.registerModItems;
+
 public class FearsMod implements ModInitializer {
 	public static final String MOD_ID = "fearsmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		ModItems.registerModItems();
+		registerModItems();
 	}
 }
--- src/main/java/net/fearsredemption/fearsmod/item/ModItems.java
+++ src/main/java/net/fearsredemption/fearsmod/item/ModItems.java
@@ -7,21 +7,23 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import static net.fearsredemption.fearsmod.FearsMod.MOD_ID;
+
 public class ModItems {
-    public static final Item VOXITE_INGOT = registerItem("voxite_ingot",
+    public static final Item MAGITEK_INGOT = registerItem("magitek_ingot",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
-    public static final Item VOXITE_NUGGET = registerItem("voxite_nugget",
+    public static final Item MAGITEK_NUGGET = registerItem("magitek_nugget",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
-    public static final Item RAW_VOXITE = registerItem("raw_voxite",
+    public static final Item RAW_MAGITEK = registerItem("raw_magitek",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registry.ITEM, new Identifier(FearsMod.MOD_ID, name), item);
+        return Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-        FearsMod.LOGGER.info("Registering Mod Items for" + FearsMod.MOD_ID);
+        FearsMod.LOGGER.info("Registering Mod Items for" + MOD_ID);
     }
 }
